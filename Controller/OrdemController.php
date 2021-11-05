e089fb43d84e10f6c8b9253088803758a0f99d42
eliseuvidal0 <eliseuvidal0@hotmail.com>
2021-11-05 19:29:44 -0300

    refatoração do codigo

--- Controller/OrdemController.php
+++ Controller/OrdemController.php
@@ -30,64 +30,34 @@ class OrdemController {
         $cpf  = preg_replace('/[^0-9]/', '', $_POST['cpf']);
         $cnpj = preg_replace('/[^0-9]/', '', $_POST['cnpj']);
 
-        if($cpf) {
+        if ($cpf) {
             $cli  = $cli->getCliente($cpf);
-        }else {
+        } else {
             $cli  = $cli->getCliente($cnpj);
         }
         
         $end  = $end->getEndereco($_POST['cep']);
         
-        if($end) {
+        if ($end) {
             $ordem->setIdEndereco($end->id_endereco);
             $endereco = $end;
         } else {
-            $end      = new EnderecoDAO();
-            $endereco->setCep($_POST['cep']);
-            $endereco->setRua($_POST['endereco']);
-            $endereco->setBairro($_POST['bairro']);
-            $endereco->setCidade($_POST['cidade']);
-            $endereco->setUf($_POST['uf']);
-            $endereco = $end->salvar($endereco);
-            $ordem->setIdEndereco($endereco->id_endereco);
+            $endereco = $this->gerarEndereco($endereco, $ordem);
         }
         
-        if($cli) {
+        if ($cli) {
             $ordem->setIdCliente($cli->id_cliente);
             $cliente = $cli;
         } else {
-            $cli      = new ClienteDAO;
-            $cliente->setNome($_POST['cliente']);
-            $cliente->setEmail($_POST['email']);
-            $cliente->setCpf($cpf);
-            $cliente->setCnpj($cnpj);
-            $cliente->setCelular($_POST['celular']);
-            $cliente->setTelefone($_POST['telefone']);
-            $cliente->setIdEndereco($endereco->id_endereco);
-            $cliente = $cli->salvar($cliente);
-            $ordem->setIdCliente($cliente->id_cliente);
+            $this->gerarCliente($cliente, $cpf, $cnpj, $endereco, $ordem);
         }
 
         $dataFormatada = implode("-", array_reverse(explode("/", trim($_POST['data']))));
         $preco = str_replace(',', '.', $_POST['preco']);
 
-        $ordem->setData($dataFormatada);
-        $ordem->setAparelho($_POST['aparelho']);
-        $ordem->setMarca($_POST['marca']);
-        $ordem->setSerie($_POST['serie']);
-        $ordem->setPreco($preco);
-        $ordem->setDefeito($_POST['defeito']);
-        $ordem->setObs($_POST['obs']);
-        $ordem->setServico($_POST['servico']);
-        $ordem->setGarantia($_POST['garantia']);
-        $ordem->setOpcao($_POST['opcao']);
+        $this->gerarOrdem($ordem, $dataFormatada, $preco);
 
-        if($this->ordemDAO->salvar($ordem)) {
-            echo "<script>alert('Ordem cadastrada com sucesso!');document.location='../index.php'</script>";
-        }else{
-            echo "<script>alert('Erro ao cadastrar Ordem!');history.back()</script>";
-        }
-        
+        $this->ordemDAO->salvar($ordem);
     }
 
     public function editar()
@@ -98,6 +68,48 @@ class OrdemController {
     public function excluir($id)
     {
         return $this->ordemDAO->excluir($id);
+    }
+
+    public function gerarEndereco($endereco, $ordem)
+    {
+        $end      = new EnderecoDAO();
+        $endereco->setCep($_POST['cep']);
+        $endereco->setRua($_POST['endereco']);
+        $endereco->setBairro($_POST['bairro']);
+        $endereco->setCidade($_POST['cidade']);
+        $endereco->setUf($_POST['uf']);
+        $endereco = $end->salvar($endereco);
+        $ordem->setIdEndereco($endereco->id_endereco);
+
+        return $endereco;
+    }
+
+    public function gerarCliente($cliente, $cpf, $cnpj, $endereco, $ordem)
+    {
+        $cli      = new ClienteDAO;
+        $cliente->setNome($_POST['cliente']);
+        $cliente->setEmail($_POST['email']);
+        $cliente->setCpf($cpf);
+        $cliente->setCnpj($cnpj);
+        $cliente->setCelular($_POST['celular']);
+        $cliente->setTelefone($_POST['telefone']);
+        $cliente->setIdEndereco($endereco->id_endereco);
+        $cliente = $cli->salvar($cliente);
+        $ordem->setIdCliente($cliente->id_cliente);
+    }
+
+    public function gerarOrdem($ordem, $dataFormatada, $preco)
+    {
+        $ordem->setData($dataFormatada);
+        $ordem->setAparelho($_POST['aparelho']);
+        $ordem->setMarca($_POST['marca']);
+        $ordem->setSerie($_POST['serie']);
+        $ordem->setPreco($preco);
+        $ordem->setDefeito($_POST['defeito']);
+        $ordem->setObs($_POST['obs']);
+        $ordem->setServico($_POST['servico']);
+        $ordem->setGarantia($_POST['garantia']);
+        $ordem->setOpcao($_POST['opcao']);
     }
 
     public function getOrdem($id)
@@ -113,10 +125,10 @@ class OrdemController {
 
 $ordemController = new OrdemController();
 
-if(isset($_GET['action']) && $_GET['action'] == 'salvar') {
+if (isset($_GET['action']) && $_GET['action'] == 'salvar') {
     $ordemController->salvar();
 }
 
-else if(isset($_GET['action']) && $_GET['action'] == 'editar') {
+else if (isset($_GET['action']) && $_GET['action'] == 'editar') {
     $ordemController->editar();
 }
